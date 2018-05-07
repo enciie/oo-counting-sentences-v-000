2df5c47dbde78eb471f58e3c403dcfb278b66dec
Nancy Jo <enciie@gmail.com>
2018-05-07 23:48:21 +0000

    automatically backed up by learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -15,7 +15,6 @@ class String
   end
 
   def count_sentences
-    self.split(/[.!?]/) 
-    self.reject { |x| x.to_s.empty? }.size
+    self.split(/[.!?]/).reject { |x| x.to_s.empty? }.size
   end
 end
